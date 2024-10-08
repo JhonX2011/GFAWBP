5b13f881fcc3316f9658aac95e2593176bb56f0d
Jhon Cano <jhonalvarez.2011@gmail.com>
2024-10-08 17:24:09 -0500

    Edit log variable name into config_controller

--- pkg/interface/controller/config_controller.go
+++ pkg/interface/controller/config_controller.go
@@ -12,7 +12,7 @@ const initialRetry int = 1
 
 type configController struct {
 	configInteractor interactor.IConfigInteractor
-	varLog           logger.Logger
+	log              logger.Logger
 	errorPresenter   presenter.IGetStructErrorPresenter
 }
 
@@ -24,26 +24,26 @@ type ConfigController interface {
 func NewConfigController(i interactor.IConfigInteractor, l logger.Logger, pe presenter.IGetStructErrorPresenter) ConfigController {
 	return &configController{
 		configInteractor: i,
-		varLog:           l,
+		log:              l,
 		errorPresenter:   pe,
 	}
 }
 
 func (p *configController) RefreshConfiguration() error {
-	p.varLog.Info("Refreshing configuration")
+	p.log.Info("Refreshing configuration")
 	if err := p.configInteractor.Reload(initialRetry); err != nil {
-		p.varLog.Error("Refreshing config", err)
+		p.log.Error("Refreshing config", err)
 		return p.errorPresenter.LoadStructError("REFRESH_ERROR", err.Error(), http.StatusInternalServerError, err, nil,
 			presenter.IsRetryableFalse)
 	}
-	p.varLog.Info("Configuration reloaded OK")
+	p.log.Info("Configuration reloaded OK")
 	return nil
 }
 
 func (p *configController) GetConfigs() (interface{}, error) {
 	response, err := p.configInteractor.GetConfigurations()
 	if err != nil {
-		p.varLog.Error("GetConfigs", err)
+		p.log.Error("GetConfigs", err)
 		return nil, p.errorPresenter.LoadStructError("DEBUG_DISABLED", err.Error(), http.StatusInternalServerError, err,
 			nil, presenter.IsRetryableFalse)
 	}
