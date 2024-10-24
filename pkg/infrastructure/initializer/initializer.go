4f45bd8b3def45d620244bd95db7e0e1e08f4059
Jhon Cano <jhonalvarez.2011@gmail.com>
2024-10-24 16:38:06 -0500

    Remove unnecessary comments

--- pkg/infrastructure/initializer/initializer.go
+++ pkg/infrastructure/initializer/initializer.go
@@ -81,30 +81,3 @@ func initGormClient(sqlDialector g.Dialector, config *mic.DBConnection) (gorm.IC
 //
 //	return httprest.NewHTTPRestProducer(metricsCoreClient).Produce(&service)
 //}
-//
-//func InitMySQLDialector(mySQLConfig *mic.MySQLConfig, connectionName string) (g.Dialector, error) {
-//	var conn database.IDBClient
-//	var dialector g.Dialector
-//	var err error
-//
-//	bytes, _ := json.Marshal(mySQLConfig)
-//	conn, err = database.NewMysqlConn(bytes)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	sqlDB, err := conn.Get(connectionName)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	dialector = mysql.New(mysql.Config{
-//		Conn: sqlDB,
-//	})
-//
-//	return dialector, err
-//}
-//
-//func InitGormClient(sqlDialector g.Dialector, config *mic.DBConnection) (gorm.IClientGorm, error) {
-//	return gorm.NewGormClient(sqlDialector, config)
-//}
